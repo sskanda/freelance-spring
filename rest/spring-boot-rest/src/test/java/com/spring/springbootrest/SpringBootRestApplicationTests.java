b63bda16465578e02caddc103722c6f0f486c6dd
Skanda <sskanda98@gmail.com>
2024-05-07 00:16:11 +0530

    rest apis

---
+++ rest/spring-boot-rest/src/test/java/com/spring/springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
