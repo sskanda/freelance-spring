44d56e4313aa7f5ec4cd75da93e257347ecad100
Skanda <sskanda98@gmail.com>
2024-05-09 22:51:06 +0530

    job api logic

--- rest/spring-boot-rest/src/main/java/com/spring/springbootrest/controller/JobRestController.java
+++ rest/spring-boot-rest/src/main/java/com/spring/springbootrest/controller/JobRestController.java
@@ -1,5 +1,26 @@
 package com.spring.springbootrest.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.springbootrest.model.JobModel;
+import com.spring.springbootrest.service.JobService;
+
+@RestController	
 public class JobRestController {
+	
+	@Autowired
+	private JobService service;
+	
+	
+	@GetMapping("jobPosts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs();
+		
+	}
 
 }
--- rest/spring-boot-rest/src/main/java/com/spring/springbootrest/model/JobModel.java
+++ rest/spring-boot-rest/src/main/java/com/spring/springbootrest/model/JobModel.java
@@ -1,5 +1,21 @@
 package com.spring.springbootrest.model;
+import java.util.List;
 
+
+import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class JobModel {
 
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private Integer reqExperience;
+	private List<String> postTechStack;
 }
